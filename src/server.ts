981ef8f11ca9f44eb5e0ec47132cee67747882f7
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2022-10-18 19:01:55 +0530

    feat: :sparkles: Auth routes setup

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import router from "./router";
 import morgan from "morgan";
 import cors from "cors";
 import { protect } from "./modules/auth";
+import { createNewUser, signIn } from "./handlers/user";
 
 const app = express();
 
@@ -18,5 +19,8 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api", protect, router);
+
+app.post("/user", createNewUser);
+app.post("/signin", signIn);
 
 export default app;
